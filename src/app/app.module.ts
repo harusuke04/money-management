85101a0b5f1c11a9d0f787ba9937c2bb0a5403ce
harusuke04 <shunsuke0404aero.ibe@gmail.com>
2021-12-30 20:27:29 +0900

    支出登録の作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InputBudgetComponent } from './input-budget/input-budget.component';
 import { HeaderComponent } from './common/header/header.component';
 import { InputIncomeComponent } from './input-income/input-income.component';
+import { InputSpendingComponent } from './input-spending/input-spending.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     InputBudgetComponent,
-    InputIncomeComponent
+    InputIncomeComponent,
+    InputSpendingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/input-spending/input-spending.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-spending',
+  templateUrl: './input-spending.component.html',
+  styleUrls: ['./input-spending.component.css']
+})
+export class InputSpendingComponent {
+}
