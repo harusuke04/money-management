502c3f4a7da2502a812b45976308809b2db305cd
harusuke04 <shunsuke0404aero.ibe@gmail.com>
2021-12-30 20:11:05 +0900

    予算追加登録の作成

---
+++ src/app/input-budget/input-budget.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-budget',
+  templateUrl: './input-budget.component.html',
+  styleUrls: ['./input-budget.component.css']
+})
+export class InputBudgetComponent {
+}
