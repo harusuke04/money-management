90397839d17858284bccf37b49a9a62f1bb1268e
harusuke04 <shunsuke0404aero.ibe@gmail.com>
2021-12-30 20:12:30 +0900

    予算追加登録、収入登録の作成変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputBudgetComponent } from './input-budget/input-budget.component';
 import { HeaderComponent } from './common/header/header.component';
+import { InputIncomeComponent } from './input-income/input-income.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InputBudgetComponent,
+    InputIncomeComponent
   ],
   imports: [
     BrowserModule,
