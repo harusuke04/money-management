38b38c6c27125b08e594a0a382d7e17bbf1a8f8f
harusuke04 <shunsuke0404aero.ibe@gmail.com>
2021-12-30 20:11:37 +0900

    収入登録の作成

---
+++ src/app/input-income/input-income.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-income',
+  templateUrl: './input-income.component.html',
+  styleUrls: ['./input-income.component.css']
+})
+export class InputIncomeComponent {
+}
